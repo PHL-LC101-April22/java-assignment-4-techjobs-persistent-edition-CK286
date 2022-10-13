4b1eedf3434e0f0706b4aa0265916a087ab97a2f
CK286 <CKing682b@gmail.com>
2022-10-12 21:18:48 -0400

    update home controller

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -45,7 +47,9 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
-     model.addAttribute("employer", employerRepository.findById(employerId));
+
+            Employer employer = employerRepository.findById(employerId).orElse(new Employer());
+            newJob.setEmployer(employer);
         return "redirect:";
     }
 
