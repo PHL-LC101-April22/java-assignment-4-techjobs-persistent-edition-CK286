76ac9084517847b9e93dd10b3afdd5607d065362
CK286 <CKing682b@gmail.com>
2022-10-15 13:55:51 -0400

    updated query 4

--- queries.sql
+++ queries.sql
@@ -14,7 +14,7 @@ DROP TABLE job;
 
 SELECT *
 FROM skill
-INNER JOIN job_skills ON skill.id = job_skills.skills_id
-WHERE job_skills.skills_id IS NOT NULL
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
 
